0ea77a9d7e79c1d8d5296448b692ae47bd54e88c
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2022-07-27 13:03:48 +0200

    Spacewar: Build custom sensors multihal
    Change-Id: I6a615458e66a7a61ec4cf8708fc613f6f50b96d0

--- device.mk
+++ device.mk
@@ -349,7 +349,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal
+    android.hardware.sensors@2.1-service.phone1-multihal \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
