14e58ca7fe9f18f39daffe9d4bf92cd476a10b3d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    Spacewar: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -375,7 +375,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
