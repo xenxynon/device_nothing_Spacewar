f7407d57b738c951b29ec915e1743c6a8a4b30bf
Aaron Kling <webgeek1234@gmail.com>
2024-09-23 02:09:02 +0900

    Spacewar: Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,6 +72,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
+        vendor/etc/media_codecs.xml|vendor/etc/media_codecs_yupik_v0.xml|vendor/etc/media_codecs_yupik_v1.xml)
+            sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
+            ;;
         vendor/lib64/libgf_hal.so)
             sed -i "s|ro.boot.flash.locked|ro.bootloader.locked|g" "${2}"
             ;;
