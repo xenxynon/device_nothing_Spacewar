9c35f066e109e26b2a3ab89977dda11763041d63
xenxynon <flynryder427@gmail.com>
2024-10-21 09:04:42 +0000

    [TEMP]Spacewar: build qti power hal for now

--- device.mk
+++ device.mk
@@ -420,7 +420,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.nothing-libperfmgr \
+    android.hardware.power-service-qti \
     android.hardware.power@1.3.vendor
 
 PRODUCT_COPY_FILES += \
