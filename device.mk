62fb3de15fcefe5308053c0f6b13525f3ea566cb
xenxynon <flynryder427@gmail.com>
2024-11-06 09:40:56 +0530

    Spacewar: build QTI health AIDL from source
    Change-Id: I4ae4bdaaadf47ff8bd692bc4d70a3c7e45608b5b
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -292,8 +292,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-Spacewar \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
