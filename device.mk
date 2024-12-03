c7a51d93b3c947c1bbe2ce42fbe717076c46c6ea
Albert I <kras@raphielgang.org>
2024-12-03 18:52:43 +0530

    Spacewar: Inherit Android Go configurations
    Change-Id: Iba8dcbe620cb211beb046a6dd97bff17150999bb
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENABLE_UFFD_GC := false
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Go Configuration
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Disable Scudo outside of eng builds to save RAM.
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
+  PRODUCT_DISABLE_SCUDO := true
+endif
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
