c4e360de9c31ff2fa0a62fa554b8aa532cf3e1bb
Michael Bestas <mkbestas@lineageos.org>
2025-01-03 13:25:06 +0000

    Spacewar: Patch libwfdservice to resolve duplicate  dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V3-cpp-source
    
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -99,6 +99,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcrypto-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "$2"
             ;;
+        system_ext/lib64/libwfdservice.so|system_ext/lib/libwfdservice.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            ;;
 
         *)
             return 1
