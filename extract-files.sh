d58197caa5c852310b260f709992cf72b291246a
xenxynon <flynryder427@gmail.com>
2025-01-03 05:27:53 +0000

    Spacewar: Sync extract-utils with template
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,12 +60,15 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/etc/init/netmgrd.rc)
+            [ "$2" = "" ] && return 0
             sed -i "/modprobe/d" "${2}"
             ;;
         vendor/lib64/hw/fingerprint.lahaina.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --set-soname "fingerprint.lahaina.so" "${2}"
             ;;
         vendor/etc/media_codecs.xml|vendor/etc/media_codecs_yupik_v0.xml|vendor/etc/media_codecs_yupik_v1.xml)
+            [ "$2" = "" ] && return 0
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d" "${2}"
             ;;
         vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
@@ -73,25 +76,37 @@ function blob_fixup() {
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
         vendor/etc/media_codecs.xml|vendor/etc/media_codecs_yupik_v0.xml|vendor/etc/media_codecs_yupik_v1.xml)
+            [ "$2" = "" ] && return 0
             sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
             ;;
         vendor/lib64/libgf_hal.so)
+            [ "$2" = "" ] && return 0
             sed -i "s|ro.boot.flash.locked|ro.bootloader.locked|g" "${2}"
             ;;
         vendor/lib64/hw/com.qti.chi.override.so)
+            [ "$2" = "" ] && return 0
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
 	vendor/etc/media_codecs.xml|vendor/etc/media_codecs_lahaina.xml|vendor/etc/media_codecs_lahaina_vendor.xml|vendor/etc/media_codecs_yupik_v1.xml)
+            [ "$2" = "" ] && return 0
             sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
             ;;
         vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so)
             [ "$2" = "" ] && return 0
             grep -q "libcrypto-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "$2"
             ;;
+
+        *)
+            return 1
+            ;;
     esac
+    return 0
+}
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
-# Initialize the helper
+# Initialize the helper.
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 if [ -z "${ONLY_FIRMWARE}" ]; then
