afe5740f8b1ef003798a8f557098019bf043692b
ImSpiDy <SpiDy2713@gmail.com>
2025-01-04 08:51:06 +0000

    Update prop override
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- pixelage_Spacewar.mk
+++ pixelage_Spacewar.mk
@@ -44,5 +44,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
 # Override device name
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=Spacewar \
-    TARGET_PRODUCT=Spacewar
+    DeviceName=Spacewar \
