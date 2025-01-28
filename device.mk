30973c91755e309a5a54216a71f9d95e5f8a2690
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-28 03:11:52 +0200

    Spacewar: implement richtap vibrator service
    Change-Id: Ieab0cfca22c0fd8a63c33ce2a6a21e982e57aee8
    
    Spacewar: Import Richtap blobs
    
    Use libaacvibrator from eqs, because ours isn't working on AOSP
    
    Change-Id: If4295efc5fd665d229bdba5757971b63ff5ef6ad
    Signed-off-by: BrainKub <fyhtjnt@gmail.com>
    
    Spacewar: import vendor namespace
    
    For libaacvibrator.
    
    Change-Id: If961a25831f3b6115171aaf6cf5b415257aa24bf
    
    Spacewar: let vibrator service access richtap dev
    
    Change-Id: I19cf413ebe128ac952e2f5237907b7bb7afcbc7f

--- device.mk
+++ device.mk
@@ -357,7 +357,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    android.hardware.vibrator.service.spacewar
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
