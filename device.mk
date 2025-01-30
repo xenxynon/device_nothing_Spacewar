6e8ef341f98dccd4717a5b19bc733f3f186f2fac
xenxynon <flynryder427@gmail.com>
2025-01-30 22:46:02 +0530

    Spacewar: include nothing camera
    Change-Id: Ia303bdc8db3a152da8304da25a95f2a91f3657d2

--- device.mk
+++ device.mk
@@ -15,6 +15,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
