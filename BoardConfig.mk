27599b548cfa3baf6e78f9001888be27c281a0ed
xenxynon <flynryder427@gmail.com>
2025-01-30 21:14:21 +0530

    Spacewar: do not enforce fcm & device manifest - Using `:=` disregards all previously included entries and only reads the ones defined in the device tree.   This can result in VINTF check errors during the zipping process. - Manually adding the missing entries later resolves the issue, but essentially replicates the ROM's predefined entries. - Switching to `+=` ensures that existing entries are preserved while including the ones defined in the device tree, avoiding conflicts and maintaining compatibility.
    Change-Id: I027347f33e7a03344a90953266ce94eee2501661

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,12 +105,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/nothing:libudfps_extension.nothing
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/qcom-battery/charging_enabled
