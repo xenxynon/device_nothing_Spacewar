c10a5c9ca589a41bcaebd1d42c6a70928230150f
xenxynon <flynryder427@gmail.com>
2025-02-06 15:05:44 +0530

    Spacewar: whitelist libencoderjpeg_jni.so  - don't preload it
    Change-Id: I456dc430920c7aa54257d0a293c632dc1c684e99

--- device.mk
+++ device.mk
@@ -180,6 +180,9 @@ PRODUCT_PACKAGES += \
     libutilscallstack.vendor:64 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.system_ext.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries.txt
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
