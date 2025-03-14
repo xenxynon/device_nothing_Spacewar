f912bbbfb5900705fba8f12e1767d91f7d1a6cf0
xenxynon <flynryder427@gmail.com>
2025-03-14 21:35:32 +0530

    Revert "Spacewar: implement richtap vibrator service"
    This reverts commit 30973c91755e309a5a54216a71f9d95e5f8a2690.
    
    Change-Id: I7595f470bc5ed89df9c227f4691d5efb48e373df

--- device.mk
+++ device.mk
@@ -374,7 +374,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator.service.spacewar
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
