af98fbcfc67b8a9699e8a2d1e3cd0e91a234e96c
xenxynon <flynryder427@gmail.com>
2025-03-15 06:19:51 +0000

    Revert "Spacewar: do not enforce fcm & device manifest"
    This reverts commit 27599b548cfa3baf6e78f9001888be27c281a0ed.
    
    Change-Id: Ib2df6fc4579152ecf4a1b754d94a3599c2b93254

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,12 +109,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nothing
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/yaap/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
