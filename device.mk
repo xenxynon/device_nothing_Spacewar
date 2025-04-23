0fffce7876762be9cdc58a6f2fdb53e873202fa3
ImSpiDy <SpiDy2713@gmail.com>
2025-04-23 15:32:35 +0000

    Spacewar: Update dex preopt boot image location
    Change-Id: Ia5c4b1fec6be98b45ce970138cf2f25c08ff79ea

--- device.mk
+++ device.mk
@@ -280,7 +280,7 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Go Configuration
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Disable Scudo outside of eng builds to save RAM.
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
