3862ede2a0554e69dd58072cd7b5340838b538a7
xenxynon <xenxynon@proton.me>
2025-10-30 03:52:59 +0000

    Spacewar: remove blob fixup for certain libs  these are kanged from qcom common with blob fixup already run for now so no point and also regenerating vendor will show weird ahh errors
    Change-Id: I84293d1aa3cfa697a00c9eb1ac6507790950f42d

--- extract-files.py
+++ extract-files.py
@@ -49,10 +49,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/lib64/libmemperfd.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-21.7.so', 'libprotobuf-cpp-lite-21.12.so'),
-    'vendor/lib64/libprekill.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full-21.7.so', 'libprotobuf-cpp-full-21.12.so'),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib/mediadrm/libwvdrmengine.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     ('vendor/lib64/libgf_hal.so'): blob_fixup()
